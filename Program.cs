232213d38a67b26c067d7de7644c9c505785129e
tejas bhavar <tejasbhavar077@gmail.com>
2024-09-23 14:48:27 +0530

    commit msg

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int harry = 5;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello Rahul");
+            Console.WriteLine("Where are you harry");
+
+
+        }
+    }
+}
